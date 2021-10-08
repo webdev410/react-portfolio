04f42f1fef18ef1e2da76e5a0b8abb8420ec6816
Andrew Keiser <webdev410@gmail.com>
2021-10-08 15:12:05 -0700

    conditional rendering is working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+import React, { useState } from "react";
+
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
 
 function App() {
+	const [currentPage, setCurrentPage] = useState("Home");
+
+	const renderPage = () => {
+		if (currentPage === "Home") {
+			return <Home></Home>;
+		}
+		if (currentPage === "About") {
+			return <About></About>;
+		}
+		if (currentPage === "Portfolio") {
+			return <Portfolio></Portfolio>;
+		}
+		if (currentPage === "Resume") {
+			return <Resume></Resume>;
+		}
+		if (currentPage === "Contact") {
+			return <Contact></Contact>;
+		}
+	};
+
+	// create handlePageChange method to set the state
+	const handlePageChange = (page) => setCurrentPage(page);
+
 	return (
 		<div className="App">
-			<Navbar></Navbar>
-			<Home></Home>
+			{/* Pass Variables through to navbar */}
+			<Navbar
+				currentPage={currentPage}
+				handlePageChange={handlePageChange}
+			></Navbar>
+			{/* Render component based off currentPage state */}
+			{renderPage()}
 		</div>
 	);
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,47 @@
 import React from "react";
 
-export default function Navbar() {
+export default function Navbar({ currentPage, handlePageChange }) {
 	return (
 		<div>
-			<h1>Navbar</h1>
+			<div className="navbar">
+				<a
+					className={
+						currentPage === "Home" ? "nav-link active" : "nav-link"
+					}
+					href="#home"
+					onClick={() => handlePageChange("Home")}
+				>
+					Home
+				</a>
+				<a
+					className="nav-link"
+					href="#about"
+					onClick={() => handlePageChange("About")}
+				>
+					About
+				</a>
+				<a
+					className="nav-link"
+					href="#portfolio"
+					onClick={() => handlePageChange("Portfolio")}
+				>
+					Portfolio
+				</a>
+				<a
+					className="nav-link"
+					href="#resume"
+					onClick={() => handlePageChange("Resume")}
+				>
+					Resume
+				</a>
+				<a
+					className="nav-link"
+					href="#contact"
+					onClick={() => handlePageChange("Contact")}
+				>
+					Contact
+				</a>
+			</div>
 		</div>
 	);
 }
