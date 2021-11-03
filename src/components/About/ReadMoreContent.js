c7094bcafb9f09591e32e45fd47c0af80c8cac60
Andrew Keiser <webdev410@gmail.com>
2021-11-03 08:56:55 -0700

    updates

--- src/components/About/ReadMoreContent.js
+++ src/components/About/ReadMoreContent.js
@@ -6,17 +6,17 @@ export default function ReadMoreContent() {
 			<p>
 				I am highly-motivated and love learning new things. I have
 				taught myself a variety of different skills and always dive in
-				passionately with both feet. Previous to completing this
-				bootcamp, I ran a successful band and was able to work as a
+				passionately with both feet. Previous to completing this boot
+				camp, I ran a successful band and was able to work as a
 				self-employed musician for 5 years. I ran a successful business
-				that grossed over $100,000 per year for 3 of those 5 years and
-				dominated our local market. I self-taught myself
-				audio-engineering at a very high level and put together a live
-				show that included complex musical technologies. We dominated
-				our local market and had many once in a lifetime experiences.
+				that grossed over $100,000 per year for 3 of those 5 years. I
+				self-taught myself audio-engineering at a very high level and
+				put together a live show that included complex musical
+				technologies. We dominated our local market and had many once in
+				a lifetime experiences.
 			</p>
 			<p>
-				I also have much experience working from home as I was an
+				I also have a lot of experience working from home as I was an
 				AppleCare At-Home Advisor in college and was allowed to work
 				from home while working for Ticketmaster. As I previously said,
 				I feel I’ve found my calling during this boot camp and I am
